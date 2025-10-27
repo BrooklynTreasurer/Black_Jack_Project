6313073b1715b7b387c70a8c2ece9cc6ed9c9043
Brooklyn <markmclean2005@gmail.com>
2025-10-27 16:44:08 +0200

    added images and a value for images

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const card = [
+    {src: "images/2_of_clubs.png", value: 2},
+    {src: "images/3_of_clubs.png", value: 3},
+    {src: "images/4_of_clubs.png", value: 4},
+    {src: "images/5_of_clubs.png", value: 5},
+    {src: "images/6_of_clubs.png", value: 6},
+    {src: "images/7_of_clubs.png", value: 7},
+    {src: "images/8_of_clubs.png", value: 8},
+    {src: "images/9_of_clubs.png", value: 9},
+    {src: "images/10_of_clubs.png", value: 10},
+    {src: "images/jack_of_clubs.png", value: 10},
+    {src: "images/queen_of_clubs.png", value: 10},
+    {src: "images/king_of_clubs.png", value: 10},
+    {src: "images/ace_of_clubs.png", value: 11},
+]
+
+
 // Blackjack game (player's cards sum)
 const messageEl = document.getElementById("message-el");
 const sumEl = document.getElementById("sum-el");
